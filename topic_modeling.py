df4af1da68592b2d278d19806310bbe0efb203d3
jeancheramy <cheramy.jean.emile@gmail.com>
2024-07-13 20:18:58 +1200

    V0.2 BERTopic modeling + data extraction to csv

--- topic_modeling.py
+++ topic_modeling.py
@@ -2,9 +2,11 @@
 import nltk
 from datetime import datetime
 import pandas as pd
+from scipy.cluster import hierarchy as sch
+from bertopic import BERTopic
 
 from bertopic.representation import MaximalMarginalRelevance
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from preprocesser import clean_text, sort_timestamps
 from bertopic import BERTopic
@@ -12,30 +14,65 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 
-# comparer diversity 0.2, 0.3, 0.4, 0.5
-# concatenate title + description as the title contains valuable information
-# display keywords and top-words for each topic
-# show topics distribution
+# topic word score all dataset
+# topics distribution in all dataset (no time info)
 # words clouds
-# topic evolution over time
+# do something with the average article length per topic (do we write less article on politics but bigger ones?)
 # numbers of articles written by journalists over time
-# use GPT to boost topics relevance
-# read all the dataset and put days limit to 60 by default
+# read all the dataset and put days limit to 180 or 365 by default
 class TopicModeler:
     def __init__(self, dataset_path, n=2000, d=60):
-        df = pd.read_csv(dataset_path, sep="\t")
+        self.model = None
+        self.dataset_path = dataset_path
+        self.texts, self.datetime_objects = self.load_dataset(n, d)
+
+    def load_dataset(self, n=2000, d=60):
+        df = pd.read_csv(self.dataset_path, sep="\t")
         titles = [clean_text(t) for t in df["Title"].tolist()[:n]]
         texts = [clean_text(t) for t in df["Text"].tolist()[:n]]
         concatenated_titles_texts = [f"{a} {b}" for a, b in zip(titles, texts)]
 
         timestamps = df["PublishDate"].tolist()[:n]
-        self.texts, self.datetime_objects = sort_timestamps(concatenated_titles_texts, timestamps, d)
+        sorted_texts, sorted_datetime_objects = sort_timestamps(concatenated_titles_texts, timestamps, d)
+        return sorted_texts, sorted_datetime_objects
 
-    def load_model(self, model_path):
-        self.model = BERTopic.load(path=model_path)
+    def load_file_model(self, model_path):
+        self.model = BERTopic.load(model_path)
 
     def save_model(self, model_path):
         self.model.save(model_path)
+
+    def extract_topics_over_time(self):
+        topics_over_time = self.model.topics_over_time(self.texts, self.datetime_objects,
+                                                       datetime_format=None,
+                                                       global_tuning=True,
+                                                       evolution_tuning=True,
+                                                       nr_bins=20)
+        fig_over_time = self.model.visualize_topics_over_time(topics_over_time, top_n_topics=20)
+        fig_over_time.write_html("figures/fig_over_time.html")
+        topics_over_time.to_csv("tableau_files/topics_over_time.csv", index=False, index_label=False)
+
+    def extract_topics_stats(self):
+        topics = self.model.get_topics()
+        print(topics)
+        dataframe_input = []
+        for top, v in topics.items():
+            for item in v:
+                d = {
+                    'TopicID': top,
+                    'TopicName': self.model.custom_labels_[top+1],
+                    'Word': item[0],
+                    'Probability': item[1]
+                }
+                dataframe_input.append(d)
+
+        new_df = pd.DataFrame(dataframe_input)
+        new_df.to_csv("tableau_files/topics_stats.csv", index=False, index_label=False)
+
+    def extract_hierarchical_topics(self):
+        linkage_function = lambda x: sch.linkage(x, 'single', optimal_ordering=True)
+        hierarchical_topics = self.model.hierarchical_topics(self.texts, linkage_function=linkage_function)
+        hierarchical_topics.to_csv("tableau_files/hierarchical_topics.csv", index=False, index_label=False)
 
     def topic_modeling(self, n):
         french_stop_words = stopwords.words('french')
@@ -53,14 +90,6 @@ class TopicModeler:
                                                          separator="_")
         self.model.set_topic_labels(topic_labels)
 
-        topics_over_time = self.model.topics_over_time(self.texts, self.datetime_objects,
-                                                        datetime_format=None,
-                                                        global_tuning=True,
-                                                        evolution_tuning=True,
-                                                        nr_bins=50)
-        fig_over_time = self.model.visualize_topics_over_time(topics_over_time, top_n_topics=20)
-        fig_over_time.write_html("fig_over_time.html")
-        topics_over_time.to_csv("powerbi_files/input_power_bi_with_outliers.csv")
         today = datetime.today()
         self.save_model(f"models/model_{n}_{today.hour}_{today.minute}_{today.day}_{today.month}_{today.year}.pickle")
 
@@ -70,3 +99,7 @@ if __name__ == "__main__":
     days = 28
     topic_modeler = TopicModeler("dataset/dataset_rtbf.csv", n, days)
     topic_modeler.topic_modeling(n)
+    topic_modeler.extract_topics_stats()
+    topic_modeler.extract_topics_over_time()
+    topic_modeler.extract_hierarchical_topics()
+    #topic_modeler.load_file_model()
